118d716e70f28d1d19fbb88ae37f222ab425c3bd
Stupel <hellomariandev@gmail.com>
2024-02-19 13:28:13 +0100

    application setup

---
+++ README.md
@@ -0,0 +1,6 @@
+## Posting App
+
+Installation.
+
+https://tailwindcss.com/docs/guides/vite
+https://redux-toolkit.js.org/introduction/getting-started
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="w-screen">
+      <h2 className="text-center text-lx my-3 font-medium">
+        Posting Application
+      </h2>
+    </div>
+  );
+}
