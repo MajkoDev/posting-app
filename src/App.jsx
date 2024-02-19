f3a48e6b0edc4c0a899fd23a07a81402ea600bb8
Stupel <hellomariandev@gmail.com>
2024-02-19 14:18:50 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import Header from "./components/Header";
+import PostForm from "./components/PostForm";
+import PostList from "./components/PostList";
 
 function App() {
   return (
     <>
       <Header />
+      <section className="m-1 rounded-md py-3 bg-slate-100">
+        <h2 className="text-lg pb-2 text-center font-semibold">Form</h2>
+        <PostForm />
+      </section>
+      <section className="m-1 rounded-md py-3 bg-slate-50">
+        <h2 className="text-lg pb-2 text-center font-semibold">List</h2>
+        <PostList />
+      </section>
     </>
   );
 }
